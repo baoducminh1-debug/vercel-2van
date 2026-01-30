6fc81ad955696ee88ceddb446f1a4c22b52bd0f2
baoducminh1-debug <baoducminh1@gmail.com>
2026-01-30 17:36:23 +0700

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,6 +1,6 @@
 const config = {
     token: '8203892450:AAHmIUrObvPw8MWA0Y1cosN2Xd4Spig-kwM',
-    chat_id: '5286758852',
+    chat_id: '-3623999784',
     noti_token: '',
     noti_chat_id: ''
 };
